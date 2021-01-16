910dff0e6ae425bcb4468ee11dc07f1c66807b9a
Micah Lund <micah.r.lund@gmail.com>
2021-01-16 13:39:00 -0700

    Set up a way to take in a path for static images and some error handling.

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const feedRoutes = require("./routes/feed");
+const path = require("path");
 
 const app = express();
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -19,12 +21,19 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
 mongoose
   .connect(
     "mongodb+srv://user1:mongo@cluster1.orslq.mongodb.net/node_complete_guide"
   )
-  .then(result => {
+  .then((result) => {
     app.listen(8080);
   })
   .catch((err) => console.log(err));
-

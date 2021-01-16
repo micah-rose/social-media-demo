db6d497883702e71e3a712136a1c1bb155ccc4df
Micah Lund <micah.r.lund@gmail.com>
2021-01-16 13:00:42 -0700

    Added some server side validation with express validator.

--- backend/routes/feed.js
+++ backend/routes/feed.js
@@ -1,13 +1,19 @@
-const express = require('express');
-
-const feedController = require('../controllers/feed');
-
+const express = require("express");
+const feedController = require("../controllers/feed");
 const router = express.Router();
+const { body } = require("express-validator/check");
 
 // GET /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get("/posts", feedController.getPosts);
 
 // POST /feed/post
-router.post('/post', feedController.createPost);
+router.post(
+  "/post",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.createPost
+);
 
 module.exports = router;

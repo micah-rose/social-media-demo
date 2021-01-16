aaa22e4d8ca42d8fb3961a22537c364a2856a22f
Micah Lund <micah.r.lund@gmail.com>
2021-01-16 13:24:55 -0700

    Stored our first post to the database/collection.

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator/check");
+const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
@@ -20,24 +21,28 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({
-        message: "Validation failed, entered data is incorrect",
-        errors: errors.array(),
-      });
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorrect",
+      errors: errors.array(),
+    });
   }
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: { name: "Max" },
-      createdAt: new Date(),
-    },
+
+  const post = new Post({
+    title: title,
+    imageUrl: 'images/picture-of-something.jpg',
+    content: content,
+    creator: { name: "Max" },
   });
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: result
+      });
+    })
+    .catch((err) => console.log(err));
 };

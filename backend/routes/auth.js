792dd547b36c802a09a21db6588a2a0e792a793e
Micah Lund <micah.r.lund@gmail.com>
2021-01-18 15:39:03 -0700

    Began setting up the signup validation for when adding a new user.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,27 @@
-const express = require('express');
+const express = require("express");
+const { body } = require("express-validator/check");
 const router = express.Router();
+const authController = require("../controllers/auth");
+const User = require("../models/user");
 
-router.put('/signup', );
+router.put(
+  "/signup",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email.")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject("E-mail address already exists.");
+          }
+        });
+      })
+      .normalizeEmail(),
+    body("password").trime().isLength({ min: 5 }),
+    body("name").trim().not().isEmpty(),
+  ],
+  authController.signup
+);
 
 module.exports = router;

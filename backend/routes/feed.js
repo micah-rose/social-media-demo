152100d7d37baf37dbf51bee91a45c03373b6a2a
Micah Lund <micah.r.lund@gmail.com>
2021-01-18 22:10:41 -0700

    Set up validation for the JSON token.

--- backend/routes/feed.js
+++ backend/routes/feed.js
@@ -2,9 +2,10 @@ const express = require("express");
 const feedController = require("../controllers/feed");
 const router = express.Router();
 const { body } = require("express-validator/check");
+const isAuth = require("../middleware/is-auth");
 
 // GET /feed/posts
-router.get("/posts", feedController.getPosts);
+router.get("/posts", isAuth, feedController.getPosts);
 
 // POST /feed/post
 router.post(
@@ -27,6 +28,6 @@ router.put(
   feedController.updatePost
 );
 
-router.delete('/post/:postId', feedController.deletePost);
+router.delete("/post/:postId", feedController.deletePost);
 
 module.exports = router;

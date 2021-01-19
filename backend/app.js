2e6113571aa084e45ad270340cc94a1df3903e02
Micah Lund <micah.r.lund@gmail.com>
2021-01-19 16:03:14 -0700

    Began removing socket.io use and routes to start using GraphQL.

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,6 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
 
-const feedRoutes = require('./routes/feed');
-const authRoutes = require('./routes/auth');
-
 const app = express();
 
 const fileStorage = multer.diskStorage({
@@ -48,9 +45,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/feed", feedRoutes);
-app.use("/auth", authRoutes);
-
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
@@ -64,10 +58,6 @@ mongoose
     "mongodb+srv://user1:mongo@cluster1.orslq.mongodb.net/node_complete_guide"
   )
   .then((result) => {
-    const server = app.listen(8080);
-    const io = require('./socket').init(server);
-    io.on('connection', socket => {
-      console.log('Client connected');
-    });
+    app.listen(8080);
   })
   .catch((err) => console.log(err));

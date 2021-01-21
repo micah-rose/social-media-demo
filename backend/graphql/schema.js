d48828684286863296418d46edc3b39d5c0aab87
Micah Lund <micah.r.lund@gmail.com>
2021-01-21 15:01:41 -0700

    Began finalizing the logic for updating a post and ran into an error with my PostInputData. My suspicion is that it's part of the resolver logic.

--- backend/graphql/schema.js
+++ backend/graphql/schema.js
@@ -44,13 +44,15 @@ module.exports = buildSchema(`
 
     type RootQuery {
         login(email: String!, password: String!): AuthData!
-        posts(page: Int!): PostData!
+        posts(page: Int): PostData!
         post(id: ID!): Post!
     }
 
     type RootMutation {
         createUser(userInput: UserInputData): User!
         createPost(postInput: PostInputData): Post!
+        updatePost(id: ID!, postInput: PostInputData): Post!
+        deletePost(id: ID!): Boolean
     }
 
     schema {

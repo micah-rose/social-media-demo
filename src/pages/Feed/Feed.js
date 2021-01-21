cbe441aa837255781ca2a35064713a0cd171ad7b
Micah Lund <micah.r.lund@gmail.com>
2021-01-20 17:29:14 -0700

    Finalized loading posts without having to refresh the page.

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -199,11 +199,21 @@ class Feed extends Component {
           creator: resData.data.createPost.creator,
           createdAt: resData.data.createPost.createdAt,
         };
-        this.setState((prevState) => {
+        this.setState(prevState => {
+          let updatedPosts = [...prevState.posts];
+          if (prevState.editPost) {
+            const postIndex = prevState.posts.findIndex(
+              p => p._id === prevState.editPost._id
+            );
+            updatedPosts[postIndex] = post;
+          } else {
+            updatedPosts.unshift(post);
+          }
           return {
+            posts: updatedPosts,
             isEditing: false,
             editPost: null,
-            editLoading: false,
+            editLoading: false
           };
         });
       })

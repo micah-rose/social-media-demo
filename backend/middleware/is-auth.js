79fc23e78184d85d8c99bb20bf67da747da5c98e
Micah Lund <micah.r.lund@gmail.com>
2021-01-21 22:11:44 -0700

    Wrote some error handling unit tests for our auth middleware.

--- backend/middleware/is-auth.js
+++ backend/middleware/is-auth.js
@@ -4,26 +4,27 @@ module.exports = (req, res, next) => {
   const authHeader = req.get("Authorization");
 
   if (!authHeader) {
-    req.isAuth = false;
-    return next();
+    const error = new Error("Not authenticated.");
+    error.statusCode = 401;
+    throw error;
   }
-  
-  const token = req.get("Authorization").split(" ")[1];
+
+  const token = authHeader.split(" ")[1];
   let decodedToken;
 
   try {
     decodedToken = jwt.verify(token, "secret");
   } catch (err) {
-    req.isAuth = false;
-    return next();
+    err.statusCode = 500;
+    throw err;
   }
 
   if (!decodedToken) {
-    req.isAuth = false;
-    return next();
+    const error = new Error("Not authenticated.");
+    error.statusCode = 401;
+    throw error;
   }
 
   req.userId = decodedToken.userId;
-  req.isAuth = true;
   next();
 };
---
+++ backend/test/auth-middleware.js
@@ -0,0 +1,24 @@
+const authMiddleware = require("../middleware/is-auth");
+const expect = require("chai").expect;
+
+describe("Auth middleware", function () {
+  it("should throw an error if no authorization header is present", function () {
+    const req = {
+      get: function () {
+        return null;
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      "Not authenticated."
+    );
+  });
+
+  it("should throw an error if the authHeader is only one string", function () {
+    const req = {
+      get: function (headerName) {
+        return "xyz";
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+});
